daaa5c9fb5197ca815b547a0ce746a710ebec6a1
johalloran <joshuamohalloran@gmail.com>
2025-06-27 15:12:34 -0400

    fixing more indentation issues

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,6 +1,6 @@
 class Meetup < ApplicationRecord
-    belongs_to :creator, class_name: "User"
-    has_many :meetup_participants, dependent: :destroy
-    has_many :participants, through: :meetup_participants, source: :user
-    has_many :meetup_availabilities, dependent: :destroy
+  belongs_to :creator, class_name: "User"
+  has_many :meetup_participants, dependent: :destroy
+  has_many :participants, through: :meetup_participants, source: :user
+  has_many :meetup_availabilities, dependent: :destroy
 end
--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -1,6 +1,6 @@
 class TimeSlot < ApplicationRecord
 	enum :day_of_week, sunday:0, monday: 1, tuesday:2, wednesday:3, thursday:4, friday:5, saturday:6
-	
+
 	has_many :user_availabilities
 	has_many :users, through: :user_availabilities
 end
