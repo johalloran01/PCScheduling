7c104cbdbe2e15b19ed583fe24dea64879be8613
johalloran <joshuamohalloran@gmail.com>
2025-06-27 15:11:17 -0400

    more reformatting

--- app/controllers/meetup_availabilities_controller.rb
+++ app/controllers/meetup_availabilities_controller.rb
@@ -1,20 +1,20 @@
 class MeetupAvailabilitiesController < ApplicationController
-    before_action :set_meetup
+  before_action :set_meetup
 
-    def create
-        time_slot_ids = params[:time_slot_ids] || []
-        #remove previous availabilities for the user/meetup
-        @meetup.meetup_availabilities.where(user: current_user).destroy_all
-        #add new availabilities
-        time_slot_ids.each do |ts_id|
-            @meetup.meetup_availabilities.create!(user: current_user, time_slot_id: ts_id)
-        end
-        redirect_to @meetup, notice: "Availability Updated"
+  def create
+    time_slot_ids = params[:time_slot_ids] || []
+    #remove previous availabilities for the user/meetup
+    @meetup.meetup_availabilities.where(user: current_user).destroy_all
+    #add new availabilities
+    time_slot_ids.each do |ts_id|
+        @meetup.meetup_availabilities.create!(user: current_user, time_slot_id: ts_id)
     end
+    redirect_to @meetup, notice: "Availability Updated"
+  end
 
-    private 
+  private 
 
-    def set_meetup
-        @meetup = Meetup.find(params[:meetup_id])
-    end
+  def set_meetup
+    @meetup = Meetup.find(params[:meetup_id])
+  end
 end
