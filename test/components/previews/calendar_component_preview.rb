a36c63068eea3559305c52f28f96ee1b1bedd393
johalloran <joshuamohalloran@gmail.com>
2025-07-10 15:08:46 -0400

    Modified some naming
    Wanted to change some variables and names for clearer labeling.

--- test/components/previews/calendar_component_preview.rb
+++ test/components/previews/calendar_component_preview.rb
@@ -4,7 +4,7 @@ class CalendarComponentPreview < ViewComponent::Preview
   def default
     week_days = TimeSlot.all.group_by { |timeslot| timeslot.day_of_week.to_sym }
 
-    time_labels = (0..23).flat_map { |h| ["%02d:00" % h, "%02d:30" % h ]}
+    time_labels = (0..23).flat_map { |hour| ["%02d:00" % hour, "%02d:30" % hour ]}
 
     render(CalendarComponent.new(week_days: week_days, time_labels: time_labels))
   end
