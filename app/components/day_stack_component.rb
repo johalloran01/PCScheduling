7519f336e9576de3c1d8cc5b7a4843100bd16ba1
johalloran <joshuamohalloran@gmail.com>
2025-07-14 12:39:52 -0400

    Finally got views to render
    Got daystacks to populate to the calendar component. It isn't structured or constrained yet but it's populating.

--- app/components/day_stack_component.rb
+++ app/components/day_stack_component.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class DayStackComponent < ViewComponent::Base
-  def initialize(timeslots: [])
+  def initialize(timeslots: [], time_labels:, day:)
     @timeslots = timeslots
+    @time_labels = time_labels
+    @day = day
   end
 end
